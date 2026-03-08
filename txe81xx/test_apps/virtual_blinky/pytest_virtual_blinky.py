6a970c98983ddd9033937de1820817dbfc3f32c4
Ronny Eia <3652665+eiaro@users.noreply.github.com>
2026-03-08 17:45:26 +0100

    Fix QEMU pytest app context and target markers

--- txe81xx/test_apps/virtual_blinky/pytest_virtual_blinky.py
+++ txe81xx/test_apps/virtual_blinky/pytest_virtual_blinky.py
@@ -2,7 +2,6 @@ import pytest
 
 
 @pytest.mark.esp32
-@pytest.mark.esp32s3
 def test_virtual_blinky(dut):
     #dut.expect(r"TXE81XX virtual mode enabled")
     dut.expect(r"TXE81XX device id: 0x[0-9A-F]{2}")
