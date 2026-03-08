065d962cbf8e42b1cc039795eb27437cdc454a5d
Ronny Eia <3652665+eiaro@users.noreply.github.com>
2026-03-08 17:32:10 +0100

    Comment out virtual mode expectation in virtual blinky test

--- txe81xx/test_apps/virtual_blinky/pytest_virtual_blinky.py
+++ txe81xx/test_apps/virtual_blinky/pytest_virtual_blinky.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.mark.esp32
 @pytest.mark.esp32s3
 def test_virtual_blinky(dut):
-    dut.expect(r"TXE81XX virtual mode enabled")
+    #dut.expect(r"TXE81XX virtual mode enabled")
     dut.expect(r"TXE81XX device id: 0x[0-9A-F]{2}")
     dut.expect(r"PORT0 output=0x01")
     dut.expect(r"PORT0 output=0x00")
